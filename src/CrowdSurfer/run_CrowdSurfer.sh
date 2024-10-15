74845b27bf24d6227adb6925250c0cb6407bd8ae
DhruvPotdar <dhruvpotdar29@gmail.com>
2024-10-15 07:36:57 +0530

    added ros package

--- src/CrowdSurfer/run_CrowdSurfer.sh
+++ src/CrowdSurfer/run_CrowdSurfer.sh
@@ -5,10 +5,10 @@ tmux new-session -d -s crowd_surfer
 tmux split-window -v
 
 tmux select-pane -t 0
-tmux send-keys "roslaunch local_dynamic_nav global_nav.launch" C-m
+tmux send-keys "roslaunch crowdsurfer_ros global_nav.launch" C-m
 
 tmux select-pane -t 1
 tmux send-keys "conda activate PRIEST" C-m
-tmux send-keys "rosrun local_dynamic_nav ros_interface_with_global_plan.py" C-m
+tmux send-keys "rosrun crowdsurfer_ros ros_interface_with_global_plan.py" C-m
 
 tmux attach-session -t crowd_surfer
